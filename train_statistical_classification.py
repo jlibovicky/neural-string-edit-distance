5fb9054413ef0d886daa2919ff1d68245f1a8381
Jindrich Libovicky <jlibovicky@gmail.com>
2020-05-25 11:38:32 +0200

    experiment management for statistical classifier

--- train_statistical_classification.py
+++ train_statistical_classification.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
+import os
 
 import torch
 from torch import nn, optim
 from torch.functional import F
 from torchtext import data
 
+from experiment import experiment_logging, get_timestamp, save_vocab
 from models import EditDistStatModel
 from transliteration_utils import decode_ids
 
@@ -15,12 +18,26 @@ def main():
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("data_prefix", type=str)
     parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--src-tokenized", default=False, action="store_true",
+                        help="If true, source side are space separated tokens.")
+    parser.add_argument("--tgt-tokenized", default=False, action="store_true",
+                        help="If true, target side are space separated tokens.")
     args = parser.parse_args()
 
+    experiment_params = (
+        args.data_prefix.replace("/", "_") +
+        "src_tokenized" if args.src_tokenized else "" +
+        "tgt_tokenized" if args.tgt_tokenized else "")
+    experiment_dir = experiment_logging(
+        f"edit_stat_{experiment_params}_{get_timestamp()}", args)
+    model_path = os.path.join(experiment_dir, "model.pt")
+
     ar_text_field = data.Field(
-        tokenize=list, init_token="<s>", eos_token="</s>", batch_first=True)
+        tokenize=(lambda s: s.split()) if args.src_tokenized else list,
+        init_token="<s>", eos_token="</s>", batch_first=True)
     en_text_field = data.Field(
-        tokenize=list, init_token="<s>", eos_token="</s>", batch_first=True)
+        tokenize=(lambda s: s.split()) if args.tgt_tokenized else list,
+        init_token="<s>", eos_token="</s>", batch_first=True)
 
     train_data, val_data, test_data = data.TabularDataset.splits(
         path=args.data_prefix, train='train.txt',
@@ -30,14 +47,22 @@ def main():
     ar_text_field.build_vocab(train_data)
     en_text_field.build_vocab(train_data)
 
+    save_vocab(
+        ar_text_field.vocab.itos, os.path.join(experiment_dir, "src_vocab"))
+    save_vocab(
+        en_text_field.vocab.itos, os.path.join(experiment_dir, "tgt_vocab"))
+    logging.info("Loaded data, created vocabularies..")
+
     train_iter, val_iter, test_iter = data.Iterator.splits(
         (train_data, val_data, test_data), batch_sizes=(1, 1, 1),
         shuffle=True, device=0, sort_key=lambda x: len(x.ar))
 
     stat_model = EditDistStatModel(ar_text_field.vocab, en_text_field.vocab)
 
+    smallest_entropy = 1e9
     pos_examples = 0
     stat_expecations = []
+    logging.info("Training starts.")
     for _ in range(args.epochs):
         for train_ex in train_iter:
             pos_examples += 1
@@ -47,8 +72,12 @@ def main():
             if pos_examples % 1000 == 999:
                 stat_model.maximize_expectation(stat_expecations)
                 entropy = -(stat_model.weights * stat_model.weights.exp()).sum()
-                print(f"stat. model entropy = {entropy.cpu():.10g}")
+                logging.info(f"stat. model entropy = {entropy.cpu():.10g}")
                 stat_expecations = []
+
+                if entropy < smallest_entropy:
+                    smallest_entropy = entropy
+                    torch.save(stat_model, model_path)
 
             if pos_examples % 1000 == 999:
                 with torch.no_grad():
@@ -59,13 +88,11 @@ def main():
                         tgt_string = decode_ids(
                             val_ex.en[0], en_text_field)
 
-                        print(f"{src_string} -> {tgt_string}   {stat_score}")
+                        logging.info(f"{src_string} -> {tgt_string}   {stat_score}")
 
                         if j > 10:
-                            print()
+                            logging.info("")
                             break
-
-    torch.save(stat_model, "stat_model.pt")
 
 
 if __name__ == "__main__":
