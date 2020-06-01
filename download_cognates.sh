44b73a2a1d4f55d2a62a1331f8b234e41c3a39a8
Jindrich Libovicky <jlibovicky@gmail.com>
2020-06-01 10:43:31 +0200

    fix cognates downloading

--- download_cognates.sh
+++ download_cognates.sh
@@ -3,15 +3,15 @@
 set -ex
 
 
-#wget https://www.aclweb.org/anthology/attachments/N18-2063.Datasets.zip
+wget https://www.aclweb.org/anthology/attachments/N18-2063.Datasets.zip
 
-# mkdir -p data/ielex
-# unzip -p N18-2063.Datasets.zip data/data-ie-42-208.tsv > data/ielex/listing.tsv
-# 
-# ./classification_data_for_cognates.py data/ielex/listing.tsv > data/ielex/all.tsv
-# head -n 20000 data/ielex/all.tsv > data/ielex/eval.txt
-# tail -n +20001 data/ielex/all.tsv | head -n 20000 > data/ielex/test.txt
-# tail -n +40001 data/ielex/all.tsv > data/ielex/train.txt
+mkdir -p data/ielex
+unzip -p N18-2063.Datasets.zip data/data-ie-42-208.tsv > data/ielex/listing.tsv
+
+./classification_data_for_cognates.py data/ielex/listing.tsv > data/ielex/all.tsv
+head -n 20000 data/ielex/all.tsv > data/ielex/eval.txt
+tail -n +20001 data/ielex/all.tsv | head -n 20000 > data/ielex/test.txt
+tail -n +40001 data/ielex/all.tsv > data/ielex/train.txt
 
 mkdir -p data/austro
 unzip -p N18-2063.Datasets.zip data/data-aa-58-200.tsv > data/austro/listing.tsv
